04d25ad17b847f8586689e2b0bcc8c30ab9ecd7f
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-10-21 22:30:38 -0700

    add config and controllers

---
+++ config/orm.js
@@ -0,0 +1,34 @@
+const connection = require("./connection");
+
+var orm = {
+    selectAll: function (tableInput) {
+        var queryString = "SELECT * FROM ??";
+        connection.query(queryString, [tableInput], function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    insertOne: function (table, cols, vals) {
+        var queryString = "INSERT INTO ? FROM ?? VALUES ??";
+
+        connection.query(queryString, [table, cols, vals], function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    updateOne: function (table, objColVals, condition) {
+        var queryString =
+            "UPDATE ? SET ? WHERE ?";
+
+        connection.query(
+            queryString,
+            [table, objColVals, condition],
+            function (err, result) {
+                if (err) throw err;
+                console.log(result);
+            }
+        );
+    }
+};
+
+module.exports = orm;
